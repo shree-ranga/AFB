480d3ee5708d546c76d3d300260ed15d3c8cb6e6
Shree Ranga Raju <shreerangaraju@gmail.com>
2021-11-12 19:06:12 +0530

    authentication urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
-# from accounts.views import Register
+from accounts.views import RegisterAPI, LoginAPI, LogoutAPI
 
-urlpatterns = []
+urlpatterns = [
+    path("register/", RegisterAPI.as_view(), name="register_user"),
+    path("login/", LoginAPI.as_view(), name="login"),
+    path("logout/", LogoutAPI.as_view(), name="logout"),
+]
